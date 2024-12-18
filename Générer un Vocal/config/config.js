32155ffc3a2936e61386722cf417ef0719e0fae6
Abdoullh621 <feddtylerblac@gmail.com>
2024-12-18 15:05:01 +0100

    Add files via upload

---
+++ Générer un Vocal/config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  discordToken: '', // توكن البوت
+  joinVoiceChannelId: '', // ايدي الروم لصنع رومك
+  categoryId: '', // ايدي الكاتوجري
+  language: 'en', // ar = للعربي | en = for english
+
+  botPermissions: [
+    'Connect',
+    'Speak',
+    'ManageChannels',
+    'MoveMembers',
+    'ViewChannel',
+    'ManageRoles',
+  ],
+
+  userPermissions: [
+    'ViewChannel',
+    'Connect',
+    'Speak',
+    'MoveMembers',
+  ],
+};
